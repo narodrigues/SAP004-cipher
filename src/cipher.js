af4dde9c51ff83f66eaf0fc0ffe6f1839bc1cea0
Natasha Gabriela Rodrigues Costa <natasha.gr.costa@gmail.com>
2020-04-02 12:24:48 -0300

    Criação das funções: cifrar e decifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,50 @@
 const cipher = {
-  // ...
+
+  encode: function(offset, string){
+    /*((codigoDaLetraASC - CódigoDaLetraA(65) + deslocamento) % 26) + CódigoDaLetraA    -> dessa forma o A se torna 0 e depois volta para o código ASC original para retornar a nova letra pós deslocamento
+      ("A".charCodeAt(0) - 65 ...)
+      string.fromCharCode(código)*/   
+    let upperCase = string.toUpperCase(); 
+    let msg = "";
+
+    for(let i = 0; i < upperCase.length; i++){
+        let originalLetter = upperCase.charCodeAt(i);
+        
+        if(originalLetter === 32){
+          msg += String.fromCharCode(originalLetter);
+          continue;
+        }
+
+        let newLetter = ((originalLetter - 65 + offset) % 26) + 65;
+        msg += String.fromCharCode(newLetter);
+    }
+    return msg;
+  },
+
+  decode: function(offset, string){
+    let upperCase = string.toUpperCase(); 
+    let msg = "";
+
+    for(let i = 0; i < upperCase.length; i++){
+      let originalLetter = upperCase.charCodeAt(i); 
+
+      if(originalLetter === 32){
+        msg += String.fromCharCode(originalLetter);
+        continue;
+      }
+      
+      let newLetter = ((originalLetter - 65 - offset)% 26);     
+          
+      if(newLetter < 0){
+          newLetter += 65 + 26;
+      } else {
+          newLetter += 65;
+      }
+
+      msg += String.fromCharCode(newLetter);
+    }
+    return msg;
+  }
 };
 
 export default cipher;
